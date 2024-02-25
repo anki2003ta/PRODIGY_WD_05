97a72575fb2e8270a26fd67eb7b53b4161a48791
Ankita Nath <145554790+anki2003ta@users.noreply.github.com>
2024-02-26 02:25:48 +0530

    2nd commit done

--- Task-1/script1.js
+++ Task-1/script1.js
@@ -10,7 +10,7 @@ const weatherIcon = document.querySelector(".weather-icon");
 async function checkWeather(city){
     const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
     
-    console.log(data);
+    /*console.log(data);*/
     if(response.status == 404){
         document.querySelector(".error").style.display = "block";
         document.querySelector(".weather").style.display = "none";
